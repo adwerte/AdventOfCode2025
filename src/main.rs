26ddd0f6e0f6484d5e2a8bbdde8cec0acc5e293c
adwerte <vikingvej@hotmail.com>
2025-12-05 19:35:38 +0100

    Revert "renamed all the files"
    This reverts commit f91d1698a17e31a01b817cfe50f10bea543afcfa.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-mod d1_safeopening;
-mod d3_battery_joltage;
-mod d4_paper_rolls;
-mod d5_shopping_ids;
+mod battery_joltage;
+mod paper_rolls;
+mod safeopening;
 fn main() {
     // Day 1
-    crate::d1_safeopening::main();
+    crate::safeopening::main();
 
     // Day 2
     //crate::invalid_ids::main();
 
     // Day 3
-    crate::d3_battery_joltage::main();
+    crate::battery_joltage::main();
 
     //Day 4
-    crate::d4_paper_rolls::main();
+    crate::paper_rolls::main();
 }
