3f840befd1ea87dad3a6ef639ebb60499cb4f56c
adwerte <vikingvej@hotmail.com>
2025-12-07 21:00:30 +0100

    DAy 7 RC start

--- src/taychon_map.rs
+++ src/taychon_map.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs, rc::Rc};
 
 pub fn main(){
     let bank = fs::read_to_string("./input/7_taychon_map.txt").unwrap();
@@ -13,6 +13,10 @@ pub fn main(){
     println!("Day 7: Splits: {}", map.splits);
 }
 
+struct TaychonPath{
+    pub parents : Vec<Rc<Self>>,
+}
+
 
 struct TaychonMap{
     splits : usize,
@@ -21,6 +25,35 @@ struct TaychonMap{
 impl TaychonMap{
     pub fn new() -> Self {
         TaychonMap { splits: 0 }
+    }
+
+    pub fn fill_row_rc(&mut self, above_row : HashMap<usize, Rc<TaychonPath>>, current_row : &str) -> HashMap<usize, Rc<TaychonPath>> {
+        let mut current_chars = current_row.chars();
+        let mut new_row: HashMap<usize, Rc<TaychonPath>> = HashMap::new();
+        let mut index = 0usize;
+        loop{
+            let parent = above_row.get(&index);
+            let current_char = current_chars.next();
+            let Some(current_char) = current_char else {
+                break;
+            };
+            let Some(parent) = parent else {
+                index += 1;
+                continue;
+            };
+            if current_char == '.' {
+
+                new_row.insert(index, Rc::new(TaychonPath { parents:  vec![parent.clone()]}));
+            } else if current_char == '^' {
+                let temp_rc = new_row.get(& (index - 1));
+                if let Some( mut temp) = temp_rc {
+                    todo!()
+                } else {
+                    new_row.insert(index - 1, Rc::new(TaychonPath { parents: vec![parent.clone()]}));
+                };
+            }
+        }
+        todo!()
     }
 
 
