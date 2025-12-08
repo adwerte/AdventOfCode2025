3748b7ff305cd86e6ee0a2e1e1fa28f99ccbdda9
adwerte <vikingvej@hotmail.com>
2025-12-08 18:38:37 +0100

    Day 7: Success!

--- src/taychon_map.rs
+++ src/taychon_map.rs
@@ -1,77 +1,60 @@
-use ndarray::{ArcArray2, Array2};
+use ndarray::Array2;
 use threadpool::ThreadPool;
 use std::sync::Arc;
-use std::{clone, collections::HashMap, fs, io::Cursor, rc::Rc, sync::mpsc, thread};
+use std::{fs, sync::mpsc};
 
 pub fn main() {
     let bank = fs::read_to_string("./input/7_taychon_map.txt").unwrap();
     let bank = bank.trim();
-    let mut map = TaychonMap::new();
     let mut splits = bank.split('\n');
-    let depth = splits.clone().count();
-    let mut rows = String::new();
-    let mut ringer = splits.next().unwrap().to_string();
-    let width = ringer.len();
+    let top_row = splits.next().unwrap();
+    let mut ringer: Vec<usize> = top_row.chars().map(|x| if x == 'S' {1} else {0}).collect();
+    let mut map = TaychonMap::new();
     for split in splits {
         //println!("{}", ringer);
-        rows.push_str(&ringer);
-        ringer = map.fill_row(&ringer, split);
+        ringer = map.fill_row(ringer, split);
     }
-    rows.push_str(&ringer);
 
     println!("Day 7: Splits: {}", map.splits);
 
-    let paths = TaychonMap::get_paths_threads(&rows, depth, width);
-
+    let paths : usize = ringer.iter().sum();
     println!("Day 7: Paths {}", paths);
     // 3334
 }
 
 struct TaychonMap {
     splits: usize,
-    paths: usize,
 }
 
 impl TaychonMap {
     pub fn new() -> Self {
         TaychonMap {
             splits: 0,
-            paths: 1,
         }
     }
 
-    pub fn fill_row(&mut self, above_row: &str, current_row: &str) -> String {
-        let mut above_chars = above_row.chars();
+    pub fn fill_row(&mut self, above_row: Vec<usize>, current_row: &str) -> Vec<usize> {
         let mut current_chars = current_row.chars();
-        let mut new_row = String::new();
-        loop {
-            let above_char = above_chars.next();
+        let mut new_row = vec![0; above_row.len()];
+        for (index, above) in above_row.iter().enumerate() {
             let current_char = current_chars.next();
-            let Some(above_char) = above_char else {
-                break;
-            };
             let Some(current_char) = current_char else {
                 break;
             };
-            if above_char == 'S' || above_char == '|' {
+            if above > &0 {
                 if current_char == '.' {
-                    new_row.push('|');
+                    new_row[index] += above;
                 } else if current_char == '^' {
-                    new_row.pop().unwrap();
-                    new_row.push('|');
-                    new_row.push('^');
-                    new_row.push('|');
+                    new_row[index - 1] += above;
+                    new_row[index + 1] += above;
                     self.splits += 1;
-                    above_chars.next();
-                    current_chars.next();
                 }
-            } else {
-                new_row.push('.');
             }
         }
 
         new_row
     }
+    #[allow(dead_code)]
     pub fn get_paths(bank: &str, depth: usize, width: usize) -> usize {
         let bank = bank.chars().collect();
         let array = Array2::from_shape_vec((depth, width), bank).unwrap();
@@ -106,7 +89,7 @@ impl TaychonMap {
 
         paths
     }
-
+    #[allow(dead_code)]
     pub fn get_paths_threads(bank: &str, depth: usize, width: usize) -> usize {
         let bank = bank.chars().collect();
         let array = Arc::new(Array2::from_shape_vec((depth, width), bank).unwrap());
@@ -135,7 +118,7 @@ impl TaychonMap {
 
         paths
     }
-
+    #[allow(dead_code)]
     fn get_rightmost_path(array: &Arc<Array2<char>>, start: [usize; 2]) -> Vec<[usize; 2]> {
         let mut cursor = start;
         let mut new_starts = Vec::new();
@@ -171,23 +154,18 @@ mod test {
         let bank = bank.trim();
         let mut map = TaychonMap::new();
         let mut splits = bank.split('\n');
-        let depth = splits.clone().count();
-        let mut ringer = splits.next().unwrap().to_string();
-        let width = ringer.len();
-        let mut rows = String::new();
+        let top_row = splits.next().unwrap();
+        let mut ringer: Vec<usize> = top_row.chars().map(|x| if x == 'S' {1} else {0}).collect();
         for split in splits {
-            println!("{}", ringer);
-            rows.push_str(&ringer);
-            ringer = map.fill_row(&ringer, split);
+            println!("{:?}", ringer);
+            ringer = map.fill_row(ringer, split);
         }
-        println!("{}", ringer);
-        rows.push_str(&ringer);
+        println!("{:?}", ringer);
 
         assert_eq!(map.splits, 21);
 
-        println!("depth: {}, {}", depth, width);
 
-        let paths = TaychonMap::get_paths_threads(&rows, depth, width);
+        let paths : usize = ringer.iter().sum();
         assert_eq!(paths, 40);
     }
     #[test]
@@ -196,23 +174,17 @@ mod test {
         let bank = bank.trim();
         let mut map = TaychonMap::new();
         let mut splits = bank.split('\n');
-        let depth = splits.clone().count();
-        let mut ringer = splits.next().unwrap().to_string();
-        let width = ringer.len();
-        let mut rows = String::new();
+        let top_row = splits.next().unwrap();
+        let mut ringer: Vec<usize> = top_row.chars().map(|x| if x == 'S' {1} else {0}).collect();
         for split in splits {
-            println!("{}", ringer);
-            rows.push_str(&ringer);
-            ringer = map.fill_row(&ringer, split);
+            println!("{:?}", ringer);
+            ringer = map.fill_row(ringer, split);
         }
-        println!("{}", ringer);
-        rows.push_str(&ringer);
+        println!("{:?}", ringer);
 
         assert_eq!(map.splits, 6);
 
-        println!("depth: {}, {}", depth, width);
-
-        let paths = TaychonMap::get_paths_threads(&rows, depth, width);
+        let paths : usize = ringer.iter().sum();
         assert_eq!(paths, 8);
     }
 
@@ -222,23 +194,17 @@ mod test {
         let bank = bank.trim();
         let mut map = TaychonMap::new();
         let mut splits = bank.split('\n');
-        let depth = splits.clone().count();
-        let mut ringer = splits.next().unwrap().to_string();
-        let width = ringer.len();
-        let mut rows = String::new();
+        let top_row = splits.next().unwrap();
+        let mut ringer: Vec<usize> = top_row.chars().map(|x| if x == 'S' {1} else {0}).collect();
         for split in splits {
-            println!("{}", ringer);
-            rows.push_str(&ringer);
-            ringer = map.fill_row(&ringer, split);
+            println!("{:?}", ringer);
+            ringer = map.fill_row(ringer, split);
         }
-        println!("{}", ringer);
-        rows.push_str(&ringer);
+        println!("{:?}", ringer);
 
         assert_eq!(map.splits, 9);
 
-        println!("depth: {}, {}", depth, width);
-
-        let paths = TaychonMap::get_paths_threads(&rows, depth, width);
-        assert_eq!(paths, 11);
+        let paths : usize = ringer.iter().sum();
+        assert_eq!(paths, 13);
     }
 }
